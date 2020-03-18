2720798ea3d5723f1c9f41097d5da35f79ad7bcf
Ivo Ivanov <45566152+ivovivanov@users.noreply.github.com>
2020-03-18 20:15:20 +0200

    Update closest_power.py

--- functions/closest_power.py
+++ functions/closest_power.py
@@ -1,3 +1,6 @@
+'''
+Write a function that returns the closest power of 'a' to 'b', where 'a' and 'b' are given integer number arguments
+'''
 def closest_power(a: int, b: int) -> int:
     result, counter = a, 1
     while result < b:
