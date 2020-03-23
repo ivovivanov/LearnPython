a05c5c68112a942027050b9012bb391f4f9247d8
Ivo Ivanov <45566152+ivovivanov@users.noreply.github.com>
2020-03-23 19:48:40 +0200

    Create list_of _squares.py

---
+++ functions/list_of _squares.py
@@ -0,0 +1,18 @@
+'''
+Function accepts random number of integers as functions arguments.
+Function should return a list of squars of the positive integers
+'''
+
+def list_of_squares(*argv):
+    result = list()
+    for arg in argv:
+        if arg > 0:
+            result.append(arg**2)
+    return result
+
+def list_of_squares_v2(*argv):
+    return [arg**2 for arg in argv if arg > 0]
+
+if __name__ == '__main__':
+    print(list_of_squares(1,3,-1,-4,-2,7,1,-8,0,9))
+    print(list_of_squares_v2(1,3,-1,-4,-2,7,1,-8,0,9))
