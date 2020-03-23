7ce96109b76470ee98da89d13946d691f3c6b239
Ivo Ivanov <45566152+ivovivanov@users.noreply.github.com>
2020-03-23 09:13:25 +0200

    Create inheritance_with_animals.py

---
+++ classes/inheritance_with_animals.py
@@ -0,0 +1,49 @@
+class animal():
+    movement = None
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return 'We have {} named {} which is {} years old.'.format(self.__class__.__name__, self.name, self.age)
+    
+    def get_old(self, additional_ages=1):
+        self.age += additional_ages
+
+    def __eq__(self, other):
+        if isinstance(other, animal): # other is animal object
+            return self.age == other.age
+        elif isinstance(other, int): #other is integer
+            return self.age == other
+        else:
+            raise TypeError("Only integers or animals are allowed")
+
+
+    
+class bird(animal):
+    movement = 'flying'
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+    def __str__(self):
+        return super().__str__() + " It is moving by {}.".format(__class__.movement)
+
+
+
+class dog(animal):
+    movement = 'running'
+    def __init__(self, name, age):
+        super().__init__(name, age)
+    def __str__(self):
+        return super().__str__() + " It is moving by {}.".format(__class__.movement)
+
+if __name__ == '__main__':
+    a = animal('Charlie', 2)
+    print(a)
+    b = dog('John', 3)
+    print(b)
+
+    p = bird('Tweety', 1)
+    print(p)
+    p.get_old()
+    print(p)
